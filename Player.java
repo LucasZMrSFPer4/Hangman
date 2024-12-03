6c018111e71a5464efa25eafc0f4d4b2ebff53be
LucasZMrSFPer4 <lucas.zhang26@student.tamdistrict.org>
2024-12-03 09:58:24 -0800

    git commit

---
+++ Player.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Player
+{
+  /* your code here - attributes */
+
+  /* your code here - constructor(s) */ 
+
+  /* your code here - accessor(s) */ 
+
+  /* your code here - mutator(s) */ 
+}
