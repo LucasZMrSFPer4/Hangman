3ec9b3fe6c73942d5b4a696264d08c9e7dab8e95
LucasZMrSFPer4 <lucas.zhang26@student.tamdistrict.org>
2025-01-19 23:36:52 -0800

    git commit

--- Board.java
+++ Board.java
@@ -8,8 +8,8 @@ import java.io.File;
 
 public class Board
 {
-  private static String solvedWord;
-  private static String word;
+  public static String solvedWord;
+  public static String word;
   private int length;
 
   public Board()
