25ecfa6abacd4271e1f88b7fd032074694bdf479
LucasZMrSFPer4 <lucas.zhang26@student.tamdistrict.org>
2025-01-19 23:25:10 -0800

    git commit

--- Board.java
+++ Board.java
@@ -8,96 +8,38 @@ import java.io.File;
 
 public class Board
 {
-  private String solvedWord;
-  private String word;
+  private static String solvedWord;
+  private static String word;
   private int currentLetterValue; 
 
   public Board()
   {
-    word = loadWord();
+    word = WordProvider.getWord();
     solvedWord = "";
-    setLetterValue();
   }
   
-  /* your code here - accessor(s) */
-  
-  /* your code here - mutator(s)  */
-
-
-  /* ---------- provided code, do not modify ---------- */
-  public void setLetterValue()
+  public String wordLength()
   {
-    int randomInt = (int) ((Math.random() * 10) + 1) * 100;    
-    currentLetterValue = randomInt;
+    int length = word.length();
+    for (int i = 0; i < length; i++)
+    {
+      solvedWord += "_";
+    }
+    return "Solved word:" + solvedWord;
   }
 
-  public boolean isSolved(String guess)
+  public void guessWord()
   {
-    if (word.equals(guess))
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter a letter: ");
+    String letter = input.nextLine();
+    int length = word.length();
+    for (int i = 0; i < length; i++)
     {
-      return true;
+      if (word.charAt(i) == letter.charAt(0))
+      {
+        solvedWord = solvedWord.substring(0, i) + letter + solvedWord.substring(i + 1);
+      }
     }
-    return false;
   }
-
-  private String loadWord()
-  {
-    String tempWord = "";
-    
-    int numOfLines = 0;
-    try 
-    {
-      Scanner sc = new Scanner(new File("wordlist.txt"));
-      while (sc.hasNextLine())
-      {
-        tempWord = sc.nextLine().trim();
-        numOfLines++;
-      }
-    } catch(Exception e) { System.out.println("Error reading or parsing wordlist.txt"); }
-    
-		int randomInt = (int) ((Math.random() * numOfLines) + 1);
-    
-    try 
-    {
-      int count = 0;
-      Scanner sc = new Scanner(new File("wordlist.txt"));
-      while (sc.hasNextLine())
-      {
-        count++;
-        String temp = sc.nextLine().trim();
-        if (count == randomInt)
-        {
-          tempWord = temp;
-        }
-      }
-    } catch (Exception e) { System.out.println("Error reading or parsing wordlist.txt"); }
-
-    for (int i = 0; i < tempWord.length(); i++)
-    {
-      solvedWord += "_ ";
-    }
-    
-    return tempWord;
-  }  
-
-  public boolean guessLetter(String guess)
-  {
-    boolean foundLetter = false;
-    String newSolvedWord = "";
-    
-    for (int i = 0; i < word.length(); i++)
-    {
-      if (word.substring(i, i + 1).equals(guess))
-      {
-        newSolvedWord += guess + " ";
-        foundLetter = true;
-      }
-      else
-      {
-        newSolvedWord += solvedWord.substring(i * 2, i * 2 + 1) + " ";  
-      }
-    }
-    solvedWord = newSolvedWord;
-    return foundLetter;
-  } 
 } 
