968a09ba0226bd6df32ef110ee83e74a3a104c6f
LucasZMrSFPer4 <lucas.zhang26@student.tamdistrict.org>
2025-02-13 23:21:00 -0800

    git commit

--- Player.java
+++ Player.java
@@ -9,7 +9,7 @@ public class Player
     System.out.println("Enter player name: ");
     Scanner input = new Scanner(System.in);
     String name = input.nextLine();
-    System.out.println("Welcome to Hangman, " + name + "!");
+    System.out.println("Welcome to Hangman, " + name + "!" + "First to 5 wins!");
   }
 
   public String getName()
