6ceabc59e3f4458a0be8c9eeedc4aa9c814b293c
LucasZSFPer4 <lucas.zhang26@student.tamdistrict.org>
2025-06-05 09:27:22 -0700

    git commit

--- WordSolver.java
+++ WordSolver.java
@@ -33,7 +33,7 @@ public class WordSolver
         System.out.println("It's your turn, " + currentPlayer + ".");
         System.out.println(board.guessWord());
 
-        if (board.letter.length() != 1 || "abcdefghijklmnopqrstuvwxyz".indexOf(board.letter) == -1 || board.usedLetters.contains(board.letter) || board.solvedWord.indexOf(board.letter) != -1)
+        if (board.letter.length() != 1 || "abcdefghijklmnopqrstuvwxyz".indexOf(board.letter) == -1 || board.usedLetters.contains(board.letter))
         {
           System.out.println("\nInvalid answer! Try again!");
           System.out.println(board.guessWord());
